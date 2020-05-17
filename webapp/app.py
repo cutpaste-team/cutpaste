7794a2f955717af0a6959c7e076eb914e5e4fad2
An <haantran96@gmail.com>
2020-05-18 00:27:59 +0300

    change deeplab to u2netp

--- webapp/app.py
+++ webapp/app.py
@@ -7,53 +7,92 @@ from plim import preprocessor
 
 from PIL import Image, ExifTags
 import numpy as np
-import tensorflow as tf
 import cv2
 
-class DeepLabModel(object):
-  """Class to load deeplab model and run inference."""
+from skimage import transform
+import torch
+import torchvision
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms#, utils
+# import torch.optim as optim
 
-  INPUT_TENSOR_NAME = 'ImageTensor:0'
-  OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
-  INPUT_SIZE = 513
-  MODEL_DIR = "./model/frozen_inference_graph.pb"
-  def __init__(self):
-    """Creates and loads pretrained deeplab model."""
-    self.graph = tf.Graph()
+from data_loader import RescaleT
+from data_loader import ToTensor
+from data_loader import ToTensorLab
+from data_loader import SalObjDataset
 
-    graph_def = None
-    # Extract frozen graph from tar archive.
-    with tf.gfile.GFile(self.MODEL_DIR, "rb") as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
+from model import U2NET # full size version 173.6 MB
+from model import U2NETP # small version u2net 4.7 MB
 
-    if graph_def is None:
-      raise RuntimeError('Cannot find inference graph in tar archive.')
+# normalize the predicted SOD probability map
+def normPRED(d):
+    ma = torch.max(d)
+    mi = torch.min(d)
 
-    with self.graph.as_default():
-      tf.import_graph_def(graph_def, name='')
+    dn = (d-mi)/(ma-mi)
 
-    self.sess = tf.Session(graph=self.graph)
+    return dn
 
-  def run(self, image):
-    """Runs inference on a single image.
+def preprocess_img(image):
+    label_3 = np.zeros(image.shape)
+    label = np.zeros(label_3.shape[0:2])
 
-    Args:
-      image: A PIL.Image object, raw input image.
+    if (3 == len(label_3.shape)):
+        label = label_3[:, :, 0]
+    elif (2 == len(label_3.shape)):
+        label = label_3
 
-    Returns:
-      resized_image: RGB image resized from original input image.
-      seg_map: Segmentation map of `resized_image`.
-    """
-    width, height = image.size
-    resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
-    target_size = (int(resize_ratio * width), int(resize_ratio * height))
-    resized_image = image.convert('RGB').resize(target_size, Image.ANTIALIAS)
-    batch_seg_map = self.sess.run(
-        self.OUTPUT_TENSOR_NAME,
-        feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
-    seg_map = batch_seg_map[0]
-    return resized_image, seg_map
+    if (3 == len(image.shape) and 2 == len(label.shape)):
+        label = label[:, :, np.newaxis]
+    elif (2 == len(image.shape) and 2 == len(label.shape)):
+        image = image[:, :, np.newaxis]
+        label = label[:, :, np.newaxis]
+
+    transform = transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
+    sample = transform({'imidx': np.array([0.]),'image': image, 'label': label})
+
+    return sample
+
+def run(img):
+    torch.cuda.empty_cache()
+    img_BGRA = None
+    with torch.no_grad():
+      sample = preprocess_img(img)
+      inputs_test = sample['image'].unsqueeze(0)
+      inputs_test = inputs_test.type(torch.FloatTensor)
+
+      if torch.cuda.is_available():
+          inputs_test = Variable(inputs_test.cuda())
+      else:
+          inputs_test = Variable(inputs_test)
+
+      d1,d2,d3,d4,d5,d6,d7= net(inputs_test)
+
+      # normalization
+      pred = d1[:,0,:,:]
+      pred = normPRED(pred)
+
+      predict = pred
+      predict = predict.squeeze()
+      predict_np = predict.cpu().data.numpy()
+
+      im = Image.fromarray(predict_np*255).convert('RGB')
+      imo = np.array(im.resize((img.shape[1],img.shape[0]),resample=Image.BILINEAR))
+
+      del d1,d2,d3,d4,d5,d6,d7
+
+      mask = np.greater(imo, imo.mean())
+      crops = img*mask
+
+      tmp = cv2.cvtColor(crops, cv2.COLOR_BGR2GRAY)
+      ret, alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+      b_channel, g_channel, r_channel = cv2.split(crops)
+      img_BGRA = cv2.merge((b_channel, g_channel, r_channel, alpha))
+    return Image.fromarray(img_BGRA)
+
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -63,25 +102,27 @@ app.config['MAKO_PREPROCESSOR'] = preprocessor
 app.config.from_object('config.ProductionConfig')
 
 print("Loading model")
-model = DeepLabModel()
+model_name='u2netp'#u2netp
+model_dir = './saved_models/'+ model_name + '/' + model_name + '.pth'
+
+if(model_name=='u2net'):
+    print("...load U2NET---173.6 MB")
+    net = U2NET(3,1)
+elif(model_name=='u2netp'):
+    print("...load U2NEP---4.7 MB")
+    net = U2NETP(3,1)
+net.load_state_dict(torch.load(model_dir,map_location=lambda storage, loc: storage))
+if torch.cuda.is_available():
+    net.cuda()
+net.eval()
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Load image
     image = request.files['file']
-    image = Image.open(image)
-    resized_im, seg_map = model.run(image)
+    image = np.array(Image.open(image))
+    img_BGRA = run(image)
 
-    mask = np.greater(seg_map, 0) # get only non-zero positive pixels/labels
-    mask = np.expand_dims(mask, axis=-1) # (H, W) -> (H, W, 1)
-    mask = np.concatenate((mask, mask, mask), axis=-1) # (H, W, 1) -> (H, W, 3), (don't like it, so if you know how to do it better, please let me know)
-    crops = resized_im * mask # apply mask on image
-
-    tmp = cv2.cvtColor(crops, cv2.COLOR_BGR2GRAY)
-    ret, alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
-    b_channel, g_channel, r_channel = cv2.split(crops)
-    img_BGRA = cv2.merge((b_channel, g_channel, r_channel, alpha))
-    img_BGRA = Image.fromarray(img_BGRA)
     byte_io = io.BytesIO()
     img_BGRA.save(byte_io, 'PNG')
     byte_io.seek(0)
